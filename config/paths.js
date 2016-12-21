5eab51088083f1915ba142d138dac70452e4f9f0
Turner Hayes <dev@turnerhayes.com>
2016-12-21 15:39:47 -0500

    Initial React branch commit

---
+++ config/paths.js
@@ -0,0 +1,7 @@
+const path = require('path');
+
+const staticPath = path.join(__dirname, '..', 'static');
+
+module.exports = {
+	"static": staticPath
+};
--- heroku-setup.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-var debug = require('debug')('webkb:heroku-setup');
-
-process.env.CONFIG_SESSION_STORE_URL = process.env.MONGOLAB_URI;
-process.env.CONFIG_DATA_STORE_URL    = process.env.MONGOLAB_URI;
